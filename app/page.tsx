bf7e5fb6d02f429c0c0277b3758d5ff7f9b9847a
abdm1513 <abskm4@gmail.com>
2024-10-15 02:06:19 -0700

    Fix image import paths to use absolute paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Cta } from "../components/Cta";
-import { Faq } from "../components/Faq";
-import { Features } from "../components/Features";
-import { Footer } from "../components/Footer";
-import { Hero } from "../components/Hero";
-import { Navbar } from "../components/Navbar";
-import { Pricing } from "../components/Pricing";
+import { Cta } from "./components/Cta";
+import { Faq } from "./components/Faq";
+import { Features } from "./components/Features";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { Navbar } from "./components/Navbar";
+import { Pricing } from "./components/Pricing";
 
 export default function Home() {
   return (
